3110cee7faecd8c226e744db6ec27d4c8773ec4d
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2023-02-25 00:41:30 -0300

    feat: add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
 
+use tracing::info;
+
 use crate::{
     disk_manager::DiskManager,
     error::{DbResult, Error},
@@ -19,6 +21,8 @@ mod pager;
 mod ioutil;
 
 fn main() -> DbResult<()> {
+    setup_tracing();
+
     let disk_manager = DiskManager::new(Path::new("ignore/my-db"))?;
     let mut pager = Pager::new(disk_manager);
 
@@ -28,12 +32,14 @@ fn main() -> DbResult<()> {
     Ok(())
 }
 
+/// Loads the first page, or bootstraps it in the case of first access.
 fn load_first_page(pager: &mut Pager) -> DbResult<FirstPage> {
     let id = PageId::new(1.try_into().unwrap());
 
     match pager.load(id) {
         Ok(first_page) => Ok(first_page),
         Err(Error::PageOutOfBounds(_)) => {
+            info!("first access; bootstrapping first page");
             let first_page = FirstPage::default();
             pager.write_flush(&first_page)?;
             Ok(first_page)
@@ -43,4 +49,22 @@ fn load_first_page(pager: &mut Pager) -> DbResult<FirstPage> {
         }
         Err(error) => Err(error),
     }
+}
+
+/// Sets up tracing subscriber.
+fn setup_tracing() {
+    use tracing_subscriber::{
+        fmt::{format::FmtSpan, layer},
+        layer::SubscriberExt,
+        util::SubscriberInitExt,
+        EnvFilter,
+    };
+
+    let filter_layer = EnvFilter::try_from_default_env().unwrap_or("warn".into());
+    let fmt_layer = layer().with_span_events(FmtSpan::NEW | FmtSpan::CLOSE);
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .init();
 }
